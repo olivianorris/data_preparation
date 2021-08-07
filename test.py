ba9846affc0ec9e4f3af7800981426cf2f212e4d
olivianorris <olivianorris56@gmail.com>
2021-08-07 17:03:43 +0900

    def checklist

--- test.py
+++ test.py
@@ -124,53 +124,51 @@ def data_summary():
     avgpool_list, coeff_list, mode='train', save_folder='datalist', save_name='')
     
     # check if the path is valid
-    def check_list(rmsks_list, rimgs_list, rlms_list, rgt_feat_list, ravgpool_list, rcoeff_list):
-        lms_list, imgs_list, msks_list, gt_feat_list, avgpool_list, coeff_list = [], [], [], [], [], []
-        for i in range(len(rlms_list)):
-            flag = 'false'
-            lm_path = rlms_list[i]
-            im_path = rimgs_list[i]
-            msk_path = rmsks_list[i]
-            gt_feat_path = rgt_feat_list[i]
-            avgpool_path = ravgpool_list[i]
-            coeff_path = rcoeff_list[i]
-            
-            if os.path.isfile(lm_path) and os.path.isfile(im_path) and os.path.isfile(msk_path) \
-                and os.path.isfile(gt_feat_path) and os.path.isfile(avgpool_path) and os.path.isfile(coeff_path):
-                flag = 'true'
-                lms_list.append(rlms_list[i])
-                imgs_list.append(rimgs_list[i])
-                msks_list.append(rmsks_list[i])
-                gt_feat_list.append(rgt_feat_list[i])
-                avgpool_list.append(ravgpool_list[i])
-                coeff_list.append(rcoeff_list[i])
-            print(i, rlms_list[i], flag)
-        return lms_list, imgs_list, msks_list, gt_feat_list, avgpool_list, coeff_list
-
-    def write_list(lms_list, imgs_list, msks_list, gt_feat_list, 
-    avgpool_list, coeff_list, mode='train',save_folder='datalist', save_name=''
-    ):
-        save_path = os.path.join(save_folder, mode)
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
-        with open(os.path.join(save_path, save_name + 'landmarks.txt'), 'w') as fd:
-            fd.writelines([i + '\n' for i in lms_list])
-    
-        with open(os.path.join(save_path, save_name + 'images.txt'), 'w') as fd:
-            fd.writelines([i + '\n' for i in imgs_list])
+def check_list(rmsks_list, rimgs_list, rlms_list, rgt_feat_list, ravgpool_list, rcoeff_list):
+    lms_list, imgs_list, msks_list, gt_feat_list, avgpool_list, coeff_list = [], [], [], [], [], []
+    for i in range(len(rlms_list)):
+        flag = 'false'
+        lm_path = rlms_list[i]
+        im_path = rimgs_list[i]
+        msk_path = rmsks_list[i]
+        gt_feat_path = rgt_feat_list[i]
+        avgpool_path = ravgpool_list[i]
+        coeff_path = rcoeff_list[i]
         
-        with open(os.path.join(save_path, save_name + 'masks.txt'), 'w') as fd:
-            fd.writelines([i + '\n' for i in msks_list])   
-    
-        with open(os.path.join(save_path, save_name + 'gt_feat.txt'), 'w') as fd:
-            fd.writelines([i + '\n' for i in gt_feat_list])   
-        
-        with open(os.path.join(save_path, save_name + 'avgpool.txt'), 'w') as fd:
-            fd.writelines([i + '\n' for i in avgpool_list])
-    
-        with open(os.path.join(save_path, save_name + 'coeff.txt'), 'w') as fd:
-            fd.writelines([i + '\n' for i in coeff_list])
+        if os.path.isfile(lm_path) and os.path.isfile(im_path) and os.path.isfile(msk_path) \
+            and os.path.isfile(gt_feat_path) and os.path.isfile(avgpool_path) and os.path.isfile(coeff_path):
+            flag = 'true'
+            lms_list.append(rlms_list[i])
+            imgs_list.append(rimgs_list[i])
+            msks_list.append(rmsks_list[i])
+            gt_feat_list.append(rgt_feat_list[i])
+            avgpool_list.append(ravgpool_list[i])
+            coeff_list.append(rcoeff_list[i])
+        print(i, rlms_list[i], flag)
+    return lms_list, imgs_list, msks_list, gt_feat_list, avgpool_list, coeff_list
+def write_list(lms_list, imgs_list, msks_list, gt_feat_list, 
+avgpool_list, coeff_list, mode='train',save_folder='datalist', save_name=''
+):
+    save_path = os.path.join(save_folder, mode)
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+    with open(os.path.join(save_path, save_name + 'landmarks.txt'), 'w') as fd:
+        fd.writelines([i + '\n' for i in lms_list])
 
+    with open(os.path.join(save_path, save_name + 'images.txt'), 'w') as fd:
+        fd.writelines([i + '\n' for i in imgs_list])
+    
+    with open(os.path.join(save_path, save_name + 'masks.txt'), 'w') as fd:
+        fd.writelines([i + '\n' for i in msks_list])   
+
+    with open(os.path.join(save_path, save_name + 'gt_feat.txt'), 'w') as fd:
+        fd.writelines([i + '\n' for i in gt_feat_list])   
+    
+    with open(os.path.join(save_path, save_name + 'avgpool.txt'), 'w') as fd:
+        fd.writelines([i + '\n' for i in avgpool_list])
+
+    with open(os.path.join(save_path, save_name + 'coeff.txt'), 'w') as fd:
+        fd.writelines([i + '\n' for i in coeff_list])
 
 if __name__ == '__main__':
     train_opt = TrainOptions().parse() #New1
